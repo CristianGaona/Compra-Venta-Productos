8824b6b4feb578ba5309f6d6c51e10d227d1b421
Cristian <crgaonas24@gmail.com>
2020-10-30 22:00:37 -0500

    Orienetando el proyecto a términos de dominio y desacoplar a que el código pertenezaca a una sola base de datos

--- src/main/java/com/crisda24/market/persistencia/ProductoRepository.java
+++ src/main/java/com/crisda24/market/persistencia/ProductoRepository.java
@@ -5,37 +5,73 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
+import com.crisda24.market.domain.Product;
+import com.crisda24.market.domain.repository.ProductRepository;
 import com.crisda24.market.persistencia.crud.ProductoCrudRepository;
 import com.crisda24.market.persistencia.entity.Producto;
+import com.crisda24.market.persistencia.mapper.ProductMapper;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
 
 	private ProductoCrudRepository productoCrudRepository;
+	private ProductMapper mapper;
 	
-	public List<Producto> getAll(){
-		return (List<Producto>) productoCrudRepository.findAll();
+	@Override
+	public List<Product> getAll(){
+		List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+		return mapper.toProducts(productos);
 	}
 	
-	public List<Producto> getByCategoria(int idCategoria){
+	
+	/*public List<Producto> getByCategoria(int idCategoria){
 		return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
 		
-	}
+	}*/
 	
-	public Optional<List<Producto>> getEscasos(int cantidad){
+
+	/*public Optional<List<Producto>> getEscasos(int cantidad){
 		return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
-	}
+	}*/
 	
-	public Optional<Producto> getProducto(int idProducto){
+
+	/*public Optional<Producto> getProducto(int idProducto){
 		return productoCrudRepository.findById(idProducto);
-	}
+	}*/
 	
-	public Producto save(Producto producto) {
+
+	/*public Producto save(Producto producto) {
 		return productoCrudRepository.save(producto);
 		
-	}
+	}*/
 	
-	public void delete(int idProducto) {
-		productoCrudRepository.deleteById(idProducto);
+    @Override
+	public void delete(int productId) {
+		productoCrudRepository.deleteById(productId);
+	}
+
+	@Override
+	public Optional<List<Product>> getByCategory(int categoryId) {
+		List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+		return Optional.of(mapper.toProducts(productos));
+	}
+
+	@Override
+	public Optional<List<Product>> getScarseProducts(int quantity) {
+		Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+		return productos.map(prods -> mapper.toProducts(prods));
+	}
+
+	@Override
+	public Optional<Product> getProduct(int productId) {
+		return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+	
+	}
+
+	@Override
+	public Product save(Product product) {
+		Producto producto = mapper.toProducto(product);
+		return mapper.toProduct(productoCrudRepository.save(producto));
+		
 	}
 }
--- src/main/java/com/crisda24/market/persistencia/mapper/ProductMapper.java
+++ src/main/java/com/crisda24/market/persistencia/mapper/ProductMapper.java
@@ -24,7 +24,7 @@ public interface ProductMapper {
 	})
 	
 	Product toProduct (Producto producto);
-	List<Product> toProducts (List<Product> products);
+	List<Product> toProducts (List<Producto> productos);
 	
 
 	@InheritInverseConfiguration
